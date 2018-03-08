b47f48cb7539598b263219549c56a5f959d0bf20
Arthur Gregorio <arthur.gregorio@pti.org.br>
2018-03-08 16:53:11 -0300

    chaged to correct password service

--- src/main/java/br/eti/arthurgregorio/shiroee/realm/JdbcSecurityRealm.java
+++ src/main/java/br/eti/arthurgregorio/shiroee/realm/JdbcSecurityRealm.java
@@ -1,6 +1,7 @@
 package br.eti.arthurgregorio.shiroee.realm;
 
 import br.eti.arthurgregorio.shiroee.auth.AuthenticationMechanism;
+import br.eti.arthurgregorio.shiroee.auth.PasswordEncoder;
 import br.eti.arthurgregorio.shiroee.config.jdbc.UserDetails;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
@@ -12,7 +13,6 @@ import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import static br.eti.arthurgregorio.shiroee.config.messages.Messages.AUTHENTICATION_ERROR;
-import org.apache.shiro.authc.credential.DefaultPasswordService;
 import org.apache.shiro.authc.credential.PasswordMatcher;
 
 /**
@@ -36,7 +36,7 @@ public class JdbcSecurityRealm extends AuthorizingRealm {
         // instantiate the custom password matcher based on bcrypt
         final PasswordMatcher passwordMatcher = new PasswordMatcher();
 
-        passwordMatcher.setPasswordService(new DefaultPasswordService());
+        passwordMatcher.setPasswordService(new PasswordEncoder());
         
         super.setCredentialsMatcher(passwordMatcher);
     }
